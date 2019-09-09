aec4fc6e89bfd858cceb111ab56ea0ba513bd467
slamet2018 <slametrifangi2018@gmailcom>
2019-09-09 11:16:18 +0700

    menambah transaksi

---
+++ application/views/transaksi2.php
@@ -0,0 +1,42 @@
+<div class="ui ordered steps">
+  <div class="completed step">
+    <div class="content">
+      <div class="title">PERTAMA</div>
+      <div class="description">step pertama</div>
+    </div>
+  </div>
+  <div class="active step">
+    <div class="content">
+      <div class="title">KEDUA</div>
+      <div class="description">step kedua</div>
+    </div>
+  </div>
+  <div class="step">
+    <div class="content">
+      <div class="title">TERAKHIR</div>
+      <div class="description">ste terakhir</div>
+    </div>
+  </div>
+</div>
+
+<div class="ui grid segment">
+  <div class="four wide column"></div>
+  <div class="ui eight wide column segment">
+    <form class="ui large form">
+      <div class="field">
+        <label class="label">HARGA/TARIF</label>
+        <h2>RP. </h2>
+        <input type="hidden" name="harga" value="" placeholder="" readonly="">
+      </div>
+      <div class="field">
+        <label class="label">TANGGAL ACARA</label>
+        <input type="date" name="tanggal" value="" placeholder="">
+      </div>
+      <div class="field">
+        <label class="label">JUMLAH PESERTA</label>
+       <input type="number" name="jumlah" value="" placeholder="">
+      </div>
+      <button name="send" class="ui blue button fluid"><i class="ui arrow right icon"></i> LANJUT</button>
+    </form>
+  </div>
+</div>
