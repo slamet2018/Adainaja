a6d111df6314a22894dd73072bec00a3b61ef92d
slamet2018 <slametrifangi2018@gmailcom>
2019-09-09 10:27:00 +0700

    add transaksi

---
+++ application/views/transaksi.php
@@ -0,0 +1,21 @@
+<div class="ui ordered steps">
+  <div class="active step">
+    <div class="content">
+      <div class="title">Shipping</div>
+      <div class="description">Choose your shipping options</div>
+    </div>
+  </div>
+  <div class="step">
+    <div class="content">
+      <div class="title">Billing</div>
+      <div class="description">Enter billing information</div>
+    </div>
+  </div>
+  <div class="step">
+    <div class="content">
+      <div class="title">Confirm Order</div>
+      <div class="description">Verify order details</div>
+    </div>
+  </div>
+</div>
+<div class="ui grid segment center aligned"></div>
