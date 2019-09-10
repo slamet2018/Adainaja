db9d4e0270ac7385d265d82832049aa8f99c5f2b
slamet2018 <slametrifangi2018@gmailcom>
2019-09-10 09:10:41 +0700

    fix footer

--- application/views/footer.php
+++ application/views/footer.php
@@ -4,7 +4,7 @@
 			<div class="ui two wide red column">
 				<h2>CALL CENTER</h2>
 				<a href="https://wa.me/6282223792709?text=saya%20mau%20tanya" class="item"><i class="ui whatsapp icon"></i>Chat melalui Whatsapp</a><br>
-				<a href="" class="item"><i class="ui envelope icon"></i>Chat melalui Email</a><br>
+				<a href="#" class="item"><i class="ui envelope icon"></i>Chat melalui Email</a><br>
 			</div>
 	</div>
 </div>
