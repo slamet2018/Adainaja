f01a479608a9df1d80deede1dcc48f36fdd5f158
slamet2018 <slametrifangi2018@gmailcom>
2019-09-10 11:27:06 +0700

    fix header

--- application/views/header.php
+++ application/views/header.php
@@ -19,21 +19,25 @@
 </head>
 <body class="site">
 	<div class="ui stackable grid">
-		<div class="ui sixteen wide blue column segment">
-			<div class="sixteen wide column segmen">	
-				<h1>ADAINAJA.COM</h1></div>
-			<div class="ui hidden divider"></div>
-			<div class="fourteen wide column">
+		<div class="ui row blue grid segment">
+			<div class="four wide column center aligned	">	
+				<h1>ADAINAJA.COM</h1>
+			</div>
+			<!-- <div class="ui hidden divider"></div> -->
+			<div class="eight wide column"></div>
+			<div class="four wide column center aligned">
 				<a href="<?php echo base_url().'login' ?>" class="ui inverted button"><i class="ui user icon"></i>login</a> | 
 				<a href="<?php echo base_url().'signup' ?>" class="ui inverted button"><i class="ui user add icon"></i>Sign up</a>
 			</div>
 		</div>
-	<div class="row">
-	<div class="ui fluid four item menu stackable grid">
-		<a href="<?php echo base_url().'' ?>" class="item"><i class="blue circular ui home icon"></i>HOME</a>
-		<a href="<?php echo base_url().'c_user/detail' ?>" class="item"><i class="blue circular ui heart icon"></i>WEDDING</a>
-		<a href="" class="item"><i class="blue circular ui birthday cake icon"></i>BIRTHDAY</a>
-		<a href="" class="item"><i class="blue circular ui building icon"></i>BUILDING ACTIVITY</a>
+	
+	
+	<!-- menu -->
+	<div class="ui row grid segment">
+		<div class="ui four wide column segment grid middle aligned">
+			<div class="eight wide column"><i class="ui circular large home icon"></i></div>
+			<div class="five wide column"><h2>HOME</h2></div>
+		</div>
 	</div>
-	</div>
-	</div>
+	<!-- akhir menu -->
+</div>
