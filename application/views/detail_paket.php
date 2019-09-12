b0048d51037b9858f9a5549a36cfc69863142073
mamankx <awanhikmawan02@gmail.com>
2019-09-12 11:03:57 +0700

    add det pket

---
+++ application/views/detail_paket.php
@@ -0,0 +1,41 @@
+	<div class="ui stackable segment">
+		<i class="address card icon"></i>
+		<div>Nama EO/Vendor</div>
+		<h4>preview paket</h4>
+		<div class="ui doubling five cards">
+			<div class="ui card">
+				<img class="ui huge image" src="<?php echo base_url().'logoaa.png' ?>">
+			</div>
+
+			<div class="ui card">
+				<img class="ui huge image" src="<?php echo base_url().'logoaa.png' ?>">
+			</div>
+			<div class="ui card">
+				<img class="ui huge image" src="<?php echo base_url().'logoaa.png' ?>">
+			</div>
+			<div class="ui card">
+				<img class="ui huge image" src="<?php echo base_url().'logoaa.png' ?>">
+			</div>
+			<div class="ui card">
+				<img class="ui huge image" src="<?php echo base_url().'logoaa.png' ?>">
+			</div>
+		</div>
+		<h4>Lokasi Pihak Terkait</h4>
+		<div><i class="map marker alternate icon"></i>Gawis Kulon Gondowulan Kepil</div><br>
+		<div><i class="dollar sign icon"></i> 120.00</div><br> 
+		<div class="ui stackable grid">
+			<div class="two wide column"></div>
+			<div class="four wide column right aligned"><a class="ui small green button" href="#"><i class="ui small shoping cart icon"></i>BOOKING NOW</a></div>
+			<div class="two wide column"></div>
+
+			<div class="two wide column"></div>
+			<div class="four wide column left aligned"><a class="ui small yellow button" href="#"><i class="ui small file icon"></i>UPLOAD BUKTI PEMBAYARAN</a></div>
+			<div class="two wide column"></div>
+		</div><br>
+		<h4>Jika sudah Order, tunggu notifikasi untuk pembayaran dan informasi lebih lanjut</h4>
+
+			<div class="ui stackable segment">
+				<div class=""><a href="#" class="ui green label"><i class="whatsapp icon"></i>Chat WhatsApp</a></div>
+				<div class=""><a href="" class="ui red label"><i class="flag outline icon"></i>Laporan Pelanggaran</a></div>
+			</div>
+	</div>
