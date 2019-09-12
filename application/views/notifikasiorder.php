143f17ec6eaa3304c38c2482264530f25f84d811
mamankx <awanhikmawan02@gmail.com>
2019-09-12 15:18:54 +0700

    fix view

--- application/views/notifikasiorder.php
+++ application/views/notifikasiorder.php
@@ -1,3 +1,67 @@
-<div class="ui sixteen wide column segment">
-	
+<div class="ui stackable segment">
+	<div class="ui grid">
+		<div class="ui three wide column left aligned">
+			<i class="address card icon"></i>
+			Nama EO/Vendor
+		</div>
+		<div class="ui three wide column left aligned">
+			<i class="address book icon"></i>
+			Nama pemilik
+		</div>
+		<div class="ui three wide column left aligned">
+			<i class="credit card outline icon"></i>
+			No.Rekening
+		</div>
+		<div class="ui five wide column"></div>
+		<div class="ui two wide column right aligned">
+			<i class="small sign-in alternate icon"></i>
+		Logout</div>
+	</div>
+<br><br>
+
+	<div>
+		<div class="ui button">
+		<a href="tambahpaket.php"><i class="ui big plus icon"></i>Tambah Paket</a>
+	</div>
+	<div class="ui button">
+		<a href="tampilpaket.php"><i class="ui big eye icon"></i>Tampil Paket</a>
+	</div>
+	<div class="ui button">
+		<a href="notifikasieo.php"><i class="ui big shopping cart icon"></i>Notifikasi Booking</a>
+	</div>
+	<div class="ui button">
+		<a href="editprofil.php"><i class="ui big edit icon"></i>Edit Profils</a>
+	</div>
+	</div>
+	<br><br>
+
+	<h3><i class="big table icon"></i>TABLE</h3>
+
+	<table border="1" class="ui eight column table">
+		<thead>
+			<tr>
+				<th>ID Paket</th>
+				<th>ID User</th>
+				<th>Nama Paket</th>
+				<th>Lokasi Pelaksanaan</th>
+				<th>Jenis Paket</th>
+				<th>Harga</th>
+				<th>Bukti Pembayaran</th>
+				<th><i class="whatsapp icon"></i>Chat orang ini</th>
+			</tr>
+		</thead>
+
+		<tbody>
+			<tr>
+				<td>001</td>
+				<td>02</td>
+				<td>Wedding</td>
+				<td>Beran</td>
+				<td>Paket Platinum</td>
+				<td>Rp 2.500.000</td>
+				<td>struk01.png</td>
+				<td>Bp.Alpha 08512345</td>
+			</tr>
+		</tbody>
+	</table>
 </div>
