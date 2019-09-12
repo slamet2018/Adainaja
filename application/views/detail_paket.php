d0a117696bd23940e8bfea1f12350986256c94aa
slamet2018 <slametrifangi2018@gmailcom>
2019-09-12 14:35:12 +0700

    fix detail pket

--- application/views/detail_paket.php
+++ application/views/detail_paket.php
@@ -23,19 +23,17 @@
 		<h4>Lokasi Pihak Terkait</h4>
 		<div><i class="map marker alternate icon"></i>Gawis Kulon Gondowulan Kepil</div><br>
 		<div><i class="dollar sign icon"></i> 120.00</div><br> 
-		<div class="ui stackable grid">
-			<div class="two wide column"></div>
-			<div class="four wide column right aligned"><a class="ui small green button" href="#"><i class="ui small shoping cart icon"></i>BOOKING NOW</a></div>
-			<div class="two wide column"></div>
-
-			<div class="two wide column"></div>
-			<div class="four wide column left aligned"><a class="ui small yellow button" href="#"><i class="ui small file icon"></i>UPLOAD BUKTI PEMBAYARAN</a></div>
-			<div class="two wide column"></div>
-		</div><br>
+		<div class="ui hidden divider"></div>
+			<div class="ui stackable container grid center aligned">
+				<div class="fourteen wide column"></div>
+				<div class="five wide column center aligned"><a href="#" class="ui green button fluid"><i class="calendar outline icon"></i>BOOKING NOW</a></div>
+				<div class="five wide column center aligned"><a href="" class="ui green button fluid"><i class="upload icon"></i>UPLOAD PEMBAYARAN</a></div>
+			</div><br>
 		<h4>Jika sudah Order, tunggu notifikasi untuk pembayaran dan informasi lebih lanjut</h4>
-
-			<div class="ui stackable segment">
-				<div class=""><a href="#" class="ui green label"><i class="whatsapp icon"></i>Chat WhatsApp</a></div>
-				<div class=""><a href="" class="ui red label"><i class="flag outline icon"></i>Laporan Pelanggaran</a></div>
+		<div class="ui hidden divider"></div>
+			<div class="ui stackable container grid center aligned">
+				<div class="fourteen wide column"></div>
+				<div class="five wide column center aligned"><a href="#" class="ui green button fluid"><i class="whatsapp icon"></i>Chat WhatsApp</a></div>
+				<div class="five wide column center aligned"><a href="" class="ui red button fluid"><i class="flag outline icon"></i>Laporan Pelanggaran</a></div>
 			</div>
 	</div>
