634390b6283f039bf09a23262823c1c650a9aed7
mamankx <awanhikmawan02@gmail.com>
2019-09-16 14:24:46 +0700

    edit c_user

--- application/controllers/c_user.php
+++ application/controllers/c_user.php
@@ -70,15 +70,13 @@ class  C_user extends CI_Controller {
 	public function notif()
 	{
 		$this->load->view('header');
-<<<<<<< HEAD
-<<<<<<< HEAD
 		$this->load->view('notif_transaksi');
-=======
+		$this->load->view('footer');
+	}
+	public function notifsign()
+	{
+		$this->load->view('header');
 		$this->load->view('notif_signin');
->>>>>>> d3de4175cc3d0b99501b561b88d2ceb01845bcc5
-=======
-		$this->load->view('notif_signin');
->>>>>>> d3de4175cc3d0b99501b561b88d2ceb01845bcc5
 		$this->load->view('footer');
 	}
 	public function konfirmasi()
