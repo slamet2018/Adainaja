9b7923802d1c12721751cc6eadfbaae0cef0a0f9
slamet2018 <slametrifangi2018@gmailcom>
2019-09-16 13:39:10 +0700

    add notif vendor

---
+++ application/views/notif_vendor.php
@@ -0,0 +1,19 @@
+<div class="ui tiny modal computer tiny modal mobile tiny modal tablet">
+    <div class="ui icon header center aligned">
+      <i class="ui huge check green icon"></i>
+      BERHASIL ['NAMA USER/VENDOR']
+      <br>
+      <br>
+       <div class="ui action">
+          <a href="<?php echo base_url().'homevendor' ?>" class="ui green ok inverted button fluid">
+        Yes
+      </a>
+    </div>
+    </div>
+  </div>
+
+<script type="text/javascript">
+  $('.tiny.modal')
+  .modal('show')
+;
+</script>
