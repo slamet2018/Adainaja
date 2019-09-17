e3478f4fa767a119fcc3ebd2c54301cfcb998ea1
slamet2018 <slametrifangi2018@gmailcom>
2019-09-17 08:34:59 +0700

    add notif user

---
+++ application/views/notif_user.php
@@ -0,0 +1,19 @@
+<div class="ui tiny modal">
+    <div class="ui icon header center aligned">
+      <i class="ui huge check green icon"></i>
+      sukses
+      <br>
+      <br>
+       <div class="ui action">
+          <a href="<?php echo base_url();?>" class="ui green ok inverted button fluid">
+        Yes
+      </a>
+    </div>
+    </div>
+  </div>
+
+<script type="text/javascript">
+  $('.tiny.modal')
+  .modal('show')
+;
+</script>
