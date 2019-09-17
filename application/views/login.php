a8d680610a0e16e0e11b93faf1b1d2623fdecaa2
slamet2018 <slametrifangi2018@gmailcom>
2019-09-17 08:36:17 +0700

    fix login

--- application/views/login.php
+++ application/views/login.php
@@ -1,12 +1,12 @@
 <div class="ui middle aligned center aligned stackable grid">
 	<div class="four wide column">
 		<div class="content"><h2>SILAHKAN LOGIN</h2></div>
-		<form class="ui large form" data-target=".tiny modal" action="<?php echo base_url().'notiflogin' ?>">
+		<form class="ui large form" data-target=".tiny modal" action="<?php echo base_url().'pilihan' ?>" method="post">
 		<div class="ui stacked segment">
 			<div class="field">
 				<div class="ui left icon input">
 					<i class="user icon"></i>
-					<input type="text" name="" value="" placeholder="USERNAME">
+					<input type="text" name="username" value="" placeholder="USERNAME">
 				</div>
 			</div>
 			<div class="field">
