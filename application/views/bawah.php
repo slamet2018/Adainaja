54f471a4c776a262a136a05c55e73cc3f078d6fe
slamet2018 <slametrifangi2018@gmailcom>
2019-09-18 15:12:30 +0700

    add bawah

---
+++ application/views/bawah.php
@@ -0,0 +1,8 @@
+<div class="ui divider"></div>
+                    <div align="center">
+                        Load in 0.3s Base build with Semantic-UI and jQuery and Author Muhibbudin Suretno
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
