add205f75371b85ace1f4e0d687dc6b1d7792b22
slamet2018 <slametrifangi2018@gmailcom>
2019-09-18 15:24:27 +0700

    fix bawah

--- application/views/bawah.php
+++ application/views/bawah.php
@@ -1,7 +1,5 @@
 <div class="ui divider"></div>
-                    <div align="center">
-                        Load in 0.3s Base build with Semantic-UI and jQuery and Author Muhibbudin Suretno
-                    </div>
+                    
                 </div>
             </div>
         </div>
