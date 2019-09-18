3f9889f629b7f952b98e8686033fd8105bf6c109
slamet2018 <slametrifangi2018@gmailcom>
2019-09-18 09:13:11 +0700

    fix home eo

--- application/views/home_eo.php
+++ application/views/home_eo.php
@@ -1,37 +1,5 @@
 
-	<div class="ui stackable segment grid">
-		<div class="ui three wide column left aligned">
-			<i class="circular address card icon"></i>
-			Nama EO/Vendor
-		</div>
-		<div class="ui three wide column left aligned">
-			<i class="circular address book icon"></i>
-			Nama pemilik
-		</div>
-		<div class="ui three wide column left aligned">
-			<i class="circular credit card outline icon"></i>
-			No.Rekening
-		</div>
-		<div class="ui five wide column"></div>
-		<div class="ui two wide column right aligned">
-			<a href="<?php echo base_url() ?>"><i class="circular small logout alternate icon"></i>
-		Logout</a></div>
-	</div>
-	<div class="ui hidden divider"></div>
-	<div class="ui container center aligned stackable grid">
-		<a href="<?php echo base_url().'tambahpaket' ?>" class="ui blue button">
-		<i class="ui big plus icon"></i>Tambah Paket
-	</a>
-	<a href="<?php echo base_url().'tampilpaket' ?>" class="ui blue button">
-		<i class="ui big eye icon"></i>Tampil Paket
-	</a>
-	<a class="ui blue button" href="<?php echo base_url().'notifikasio' ?>">
-		<i class="ui big shopping cart icon"></i>Notifikasi Booking
-	</a>
-	<button class="ui blue button">
-		<i class="ui big edit icon"></i>Edit Profils
-	</button>
-	</div>
+	
 	<div class="ui hidden divider"></div>
 	<div class="ui segment grid ">
 		<div class="sixteen wide column container">
