0a061e4755b91e6394e2d738a8ee6718cdb4247f
slamet2018 <slametrifangi2018@gmailcom>
2019-09-18 09:12:27 +0700

    fix menu vendor

---
+++ application/views/menuvendor.php
@@ -0,0 +1,42 @@
+
+	<div class="ui stackable segment grid">
+		<div class="ui three wide column left aligned">
+			<i class="circular address card icon"></i>
+			Nama EO/Vendor
+		</div>
+		<div class="ui three wide column left aligned">
+			<i class="circular address book icon"></i>
+			Nama pemilik
+		</div>
+		<div class="ui three wide column left aligned">
+			<i class="circular credit card outline icon"></i>
+			No.Rekening
+		</div>
+		<div class="ui five wide column"></div>
+		<div class="ui two wide column right aligned">
+			<a href="<?php echo base_url() ?>"><i class="circular small logout alternate icon"></i>
+		Logout</a></div>
+	</div>
+<div class="ui hidden divider"></div>
+	<div class="ui container center aligned stackable grid">
+		<div class="four wide column">
+			<a href="<?php echo base_url().'tambahpaket' ?>" class="ui blue button fluid">
+				<i class="ui big plus icon"></i>Tambah Paket
+			</a>
+		</div>
+		<div class="four wide column">
+			<a href="<?php echo base_url().'tampilpaket' ?>" class="ui blue button fluid">
+				<i class="ui big eye icon"></i>Tampil Paket
+			</a>
+		</div>
+		<div class="four wide column">
+			<a class="ui blue button fluid" href="<?php echo base_url().'notifikasio' ?>">
+				<i class="ui big shopping cart icon"></i>Notifikasi Booking
+			</a>
+		</div>
+		<div class="four wide column">
+			<a href="#" class="ui blue button fluid">
+				<i class="ui big edit icon"></i>Edit Profils
+			</a>
+		</div>
+	</div>
