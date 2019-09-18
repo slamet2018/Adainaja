b7f8f2926ea5ee00436cf53b50cc37c4ea8519c2
slamet2018 <slametrifangi2018@gmailcom>
2019-09-18 09:13:25 +0700

    fix n order

--- application/views/notifikasiorder.php
+++ application/views/notifikasiorder.php
@@ -1,38 +1,3 @@
-<div class="ui stackable segment">
-	<div class="ui grid">
-		<div class="ui three wide column left aligned">
-			<i class="address card icon"></i>
-			Nama EO/Vendor
-		</div>
-		<div class="ui three wide column left aligned">
-			<i class="address book icon"></i>
-			Nama pemilik
-		</div>
-		<div class="ui three wide column left aligned">
-			<i class="credit card outline icon"></i>
-			No.Rekening
-		</div>
-		<div class="ui five wide column"></div>
-		<a class="ui two wide column right aligned" href="<?php echo base_url() ?>">
-			<i class="small sign-in alternate icon"></i>
-		Logout</a>
-	</div>
-<br><br>
-
-	<div>
-		<div class="ui button">
-		<a href="tambahpaket.php"><i class="ui big plus icon"></i>Tambah Paket</a>
-	</div>
-	<div class="ui button">
-		<a href="tampilpaket.php"><i class="ui big eye icon"></i>Tampil Paket</a>
-	</div>
-	<div class="ui button">
-		<a href="notifikasieo.php"><i class="ui big shopping cart icon"></i>Notifikasi Booking</a>
-	</div>
-	<div class="ui button">
-		<a href="editprofil.php"><i class="ui big edit icon"></i>Edit Profils</a>
-	</div>
-	</div>
 	<br><br>
 
 	<h3><i class="big table icon"></i>TABLE</h3>
