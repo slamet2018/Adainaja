0a8dd85d78a6cf9c196b7eff345721fc879ba2f3
mamankx <awanhikmawan02@gmail.com>
2019-09-18 14:04:33 +0700

    tampil paket

--- application/views/tampilpaket.php
+++ application/views/tampilpaket.php
@@ -1,53 +1,44 @@
+<br><br><br>
 <div>
 		<table class="ui unstackable table">
   <thead>
     <tr>
-      <th>Name</th>
-      <th>Status</th>
-      <th class="right aligned">Notes</th>
+      <th>Nama Paket</th>
+      <th>Jenis Paket</th>
+      <th>Harga</th>
+      <th>Gambar Preview 1</th>
+      <th>Gambar Preview 2</th>
+      <th>Gambar Preview 3</th>
+      <th>Gambar Preview 4</th>
     </tr>
   </thead>
   <tbody>
     <tr>
-      <td>John</td>
-      <td>Approved</td>
-      <td class="right aligned">None</td>
+      <td>Paket Gold</td>
+      <td>Wedding</td>
+      <td>Rp 300.000</td>
+      <td>gambar1.png</td>
+      <td>gambar2.png</td>
+      <td>gambar3.png</td>
+      <td>gambar4.png</td>
     </tr>
     <tr>
-      <td>Jamie</td>
-      <td>Approved</td>
-      <td class="right aligned">Requires call</td>
+      <td>Paket Silver</td>
+      <td>Birthday</td>
+      <td>Rp 200.000</td>
+      <td>gambar1.png</td>
+      <td>gambar2.png</td>
+      <td>gambar3.png</td>
+      <td>gambar4.png</td>
     </tr>
     <tr>
-      <td>Jill</td>
-      <td>Denied</td>
-      <td class="right aligned">None</td>
-    </tr>
-  </tbody>
-</table>
-<table class="ui tablet stackable table">
-  <thead>
-    <tr>
-      <th>Name</th>
-      <th>Status</th>
-      <th class="right aligned">Notes</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <td>John</td>
-      <td>Approved</td>
-      <td class="right aligned">None</td>
-    </tr>
-    <tr>
-      <td>Jamie</td>
-      <td>Approved</td>
-      <td class="right aligned">Requires call</td>
-    </tr>
-    <tr>
-      <td>Jill</td>
-      <td>Denied</td>
-      <td class="right aligned">None</td>
+      <td>Paket Platinum</td>
+      <td>Building Act</td>
+      <td>Rp 350.000</td>
+      <td>gambar1.png</td>
+      <td>gambar2.png</td>
+      <td>gambar3.png</td>
+      <td>gambar4.png</td>
     </tr>
   </tbody>
 </table>
