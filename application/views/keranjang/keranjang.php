40bf85958eac7fb8be35b5aa03565eef53f49193
slamet <slametrifangi2018@gmail.com>
2019-09-23 14:09:02 +0700

    add folder keranjang

---
+++ application/views/keranjang/keranjang.php
@@ -0,0 +1,16 @@
+<div class="ui grid center aligned">
+	<div class="ui nine wide inverted red column middle aligned grid">
+		<div class="one wide column right floated">
+			<script>
+    			document.write('<a href="' + document.referrer + '" class="ui red label"><i class="ui arrow left icon"></i></a>');
+			</script>
+		</div>
+		<div class="fourteen wide column left aligned">
+			<h3>Keranjang</h3>
+		</div>
+	</div>
+		<div class="ui nine wide column left aligned merahm grid">
+			<div class="fourteen wide column">hehe</div>
+			<div class="two wide column right floated"><i class="ui angle right icon"></i></div>
+		</div>
+</div>
