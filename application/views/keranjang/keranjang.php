7517873609b7f79b590c5523ff068dcc10f43a3a
slamet <slametrifangi2018@gmail.com>
2019-10-01 08:31:50 +0700

    add keranjang transaksi

--- application/views/keranjang/keranjang.php
+++ application/views/keranjang/keranjang.php
@@ -1,16 +1,28 @@
 <div class="ui grid center aligned">
-	<div class="ui nine wide inverted red column middle aligned grid">
+	<div class="ui nine wide inverted black column middle aligned grid">
 		<div class="one wide column right floated">
 			<script>
-    			document.write('<a href="' + document.referrer + '" class="ui red label"><i class="ui arrow left icon"></i></a>');
+    			document.write('<a href="' + document.referrer + '" class="item"><i class="ui arrow left icon"></i></a>');
 			</script>
 		</div>
 		<div class="fourteen wide column left aligned">
 			<h3>Keranjang</h3>
 		</div>
 	</div>
-		<div class="ui nine wide column left aligned merahm grid">
-			<div class="fourteen wide column">hehe</div>
+		<div class="ui nine wide column left aligned grid">
+			<div class="four wide column">Tenda Pernikahan</div>
 			<div class="two wide column right floated"><i class="ui angle right icon"></i></div>
+			<div class="ui header divider"></div>
+			<div class="sixteen wide column grid">
+				<img class="ui top aligned small image" src="<?php echo base_url().'image/weding.jpg' ?>">
+				<span>Top Aligned</span>
+			</div>
+			<div class="ui header divider"></div>
+			<div class="ui sixteen wide inverted black column grid center aligned middle aligned container stackable">
+				<div class="ten wide column left aligned middle aligned">
+					<input type="checkbox" class="ui checkbox" name="" value="">Pilih Semua
+				</div>
+				<div class="five wide column "><a href="" class="ui red button">Checkout</a></div>
+			</div>
 		</div>
 </div>
