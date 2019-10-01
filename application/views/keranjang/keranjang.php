7fd90617b5a3a82bdd7c2476880f33647588eab7
slamet <slametrifangi2018@gmail.com>
2019-10-01 11:18:41 +0700

    fix keranjang

--- application/views/keranjang/keranjang.php
+++ application/views/keranjang/keranjang.php
@@ -1,12 +1,25 @@
 <div class="container">
 	<div class="row">
 		<div class="col-xs-12">
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			<p class="navbar-text">
+				<div class="checkbox">
+					<label>
+						<input type="checkbox" name="" value="">Detail Produk <span class="glyphicon glyphicon-chevron-right"></span>
+					</label>
+				</div>
+			</p>
+		</div>
+	</div>
+
+</div>
+<div class="container">
+	<div class="row">
+		<div class="col-xs-5 col-sm-2">
+			<img src="<?php echo base_url().'image/ultah.jpg' ?>" alt="" class="img-thumnail img-responsive">
+		</div>
+		<div class="col-xs-7 col-sm-10">
+			<p>KETERANGAN PRODUK</p>
 		</div>
 	</div>
 </div>
+<hr>
