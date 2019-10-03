fbe9fb27de0c423202d50da815b86c5e32b380f3
mamankx <awanhikmawan02@gmail.com>
2019-10-03 15:38:03 +0700

    new stats trans

---
+++ application/views/status_transaksi.php
@@ -0,0 +1,23 @@
+<br>
+<div class="ui grid">
+	<div class="ui sixteen teal column">
+		<div class="">
+			<h3><i class="address card icon"></i>Nama Vendor</h3>
+			<h4><i class="shopping basket icon"></i>Nama Paket</h4>
+		</div>
+	</div>
+</div>
+<br><br>
+<div class="ui grid">
+	<div class="ui sixteen column">
+		<div class="">
+			<h3>STATUS TRANSAKSI</h3>
+		</div><hr>
+		<div class="ui segment container">
+			<div><i class="circle outline icon"></i>Processing</div><hr>
+			<div><i class="circle icon"></i>Accepted by Vendor</div><hr>
+			<div><i class="circle icon"></i>Pesanan Terkirim ke Vendor</div><hr>
+			<div><i class="circle icon"></i>Pesanan Dibuat</div>
+		</div>
+	</div>
+</div>
