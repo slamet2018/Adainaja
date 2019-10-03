e19fce78afbaf393ee7b3195db7cbc542f0fe62a
slamet <slametrifangi2018@gmail.com>
2019-10-03 15:12:05 +0700

    fix header

--- application/views/keranjang/header_transaksi.php
+++ application/views/keranjang/header_transaksi.php
@@ -15,19 +15,38 @@
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
+    <script type="text/javascript">
+      function myFunction() {
+  /* Get the text field */
+  var copyText = document.getElementById("myInput");
+
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /*For mobile devices*/
+
+  /* Copy the text inside the text field */
+  document.execCommand("copy");
+
+  /* Alert the copied text */
+  alert("Copied the text" + copyText.value);
+}
+    </script>
   </head>
   <body>
 <nav class="navbar navbar-default navbar-static-top" role="navigation">
 <div class="container">
   	<div class="row">
-  		<div class="col-xs-3 text-center">
+  		<div class="col-xs-2 text-center">
   			<ul class="pager">
- 				 <li class="previous"><a href="<?php echo base_url() ?>">&larr; Older</a></li>
+ 				 <li class="previous"><a href="<?php echo base_url() ?>">&larr; home</a></li>
 			</ul>
   		</div>
-  		<div class="col-xs-4 text-left">
- 				 <h2>KERANJANG</h2>
+  		<div class="col-xs-6 text-left">
+ 				 <h2> KERANJANG</h2>
   		</div>
+      <div class="col-xs-4 justify-content-left">
+        <h2>Adainaja</h2>
+      </div>
   	</div>
   </div>
 </nav>
