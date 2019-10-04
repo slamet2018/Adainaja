14d715c99c3d726efbbdc22fd840e0b70d145061
mamankx <awanhikmawan02@gmail.com>
2019-10-04 10:09:44 +0700

    add daftqar trans

---
+++ application/views/daftar_transaksi.php
@@ -0,0 +1,16 @@
+<br>
+<div class="ui grid">
+	<div class="ui sixteen wide teal column">
+		<div>
+			<h3>DAFTAR TRANSAKSI</h3>
+			<h4>Nama User</h4>
+		</div>
+		<div class="ui stackable container segment grid">
+			<h4 class="twelve wide column left aligned" style="color: black"><i class="address card icon"></i>Transaksi kepada vendor A</h4>
+			<a class="four wide column center aligned" href="#">Detail Status</a>
+			
+			<h4 class="twelve wide column left aligned" style="color: black"><i class="address card icon"></i>Transaksi kepada vendor C</h4>
+			<a class="four wide column center aligned" href="#">Detail Status</a>
+		</div>
+	</div>
+</div>
